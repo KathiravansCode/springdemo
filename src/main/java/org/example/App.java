281bd9508e27f6bb345cd8450e24be3d41388ea3
KathiravansCode <kathiravanprogrammer@gmail.com>
2025-03-17 22:52:56 +0530

    spring demo finished

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,8 +12,8 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext  context=new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj1=(Alien) context.getBean("alien1");
-
+//        Alien obj1=(Alien) context.getBean("alien1");
+        Alien obj1=context.getBean(Alien.class);
         obj1.code();
         //          obj1.age=21;
         System.out.println(obj1.getAge());
